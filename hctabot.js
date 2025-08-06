fbb2023acdb1f06ff606ce0d21d49fbe772f0bb7
Filipi Neves <107006848+FilipiPNeves@users.noreply.github.com>
2025-08-06 10:57:28 -0300

    first commit

---
+++ hctabot.js
@@ -0,0 +1,45 @@
+const { default: makeWASocket, useMultiFileAuthState, DisconnectReason } = require('@whiskeysockets/baileys');
+const { Boom } = require('@hapi/boom');
+
+async function startBot() {
+    const { state, saveCreds } = await useMultiFileAuthState('auth_info_baileys');
+    const sock = makeWASocket({
+        auth: state
+    });
+
+    sock.ev.on('creds.update', saveCreds);
+
+    // Set para guardar os usuários já respondidos
+    const respondedUsers = new Set();
+
+    sock.ev.on('connection.update', (update) => {
+        const { connection, lastDisconnect, qr } = update;
+        if (qr) {
+            console.log('QR code:', qr);
+        }
+        if(connection === 'close') {
+            const shouldReconnect = (lastDisconnect.error = new Boom(lastDisconnect?.error))?.output?.statusCode !== DisconnectReason.loggedOut;
+            if(shouldReconnect) {
+                startBot();
+            }
+        } else if(connection === 'open') {
+            console.log('Bot conectado!');
+        }
+    });
+
+    sock.ev.on('messages.upsert', async ({ messages, type }) => {
+        if(type === 'notify') {
+            const msg = messages[0];
+            if(!msg.key.fromMe && msg.message) {
+                // Para grupos, use participant; para privado, use remoteJid
+                const userJid = msg.key.participant || msg.key.remoteJid;
+                if (!respondedUsers.has(userJid)) {
+                    await sock.sendMessage(msg.key.remoteJid, { text: 'Olá, tudo bem? \n\nPara eu lhe informar valores, me diga:\n\n- Qual data de entrada?\n\n- Qual data de saída?\n\n- Quantas pessoas?' });
+                    respondedUsers.add(userJid);
+                }
+            }
+        }
+    });
+}
+
+startBot();
